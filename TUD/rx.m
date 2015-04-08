bfef1d311457db3dcf63409bf86a3ec169935e9a
martin.danneberg.git <martin.danneberg@ifn.et.tu-dresden.de>
2015-04-08 10:28:18 +0200

    TUD added basic RX example.

---
+++ TUD/rx.m
@@ -0,0 +1,15 @@
+%% init TestMan
+% load default HaLo config
+[ halo ] = halo_defaultConfig('halo');
+
+%% init RX Hardware    
+halo_init_rx(halo); 
+
+%% Receive signal
+rec = halo_getSignal(halo);
+% It could be empty, if something went wrong
+if (~isempty(rec))
+    figure(1);
+    spectrum = 10*log10(abs(fft(rec)));
+    plot(spectrum);
+end
